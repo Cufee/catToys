f926ed0e6b9ec76b45f5bf787ba7616a9299d7c7
Cufee <vkouzin@outlook.com>
2020-03-30 15:46:49 -0700

    Added Groups parsing

--- apiRunner.js
+++ apiRunner.js
@@ -1,5 +1,5 @@
 //Version
-var scriptVersion = 'v0.2';
+var scriptVersion = 'v0.2.2';
 document.getElementById('app-version').innerHTML = scriptVersion;
 //API url
 var apiUrl = 'https://mixer.com/api/v1/channels/';
@@ -22,9 +22,12 @@ function Get(url) {
     var statusCode = JSON.parse(response)['statusCode']
     console.log('Status Code ' + statusCode)
     if (statusCode == 404) {
-        alert("User not found, API returned 404");
+        var badResponse = '{"statusCode":404}';
+        console.log(badResponse)
+        return badResponse;
     }
     else {
+        console.log(typeof(response))
         return response.toString();
     }   
 }
@@ -34,6 +37,8 @@ function parseLightstream(channelId) {
     console.log('Parsing Lighstream')
     videoUrl = apiUrl + channelId.toString() + '/videoSettings';
     var rawData = JSON.parse(Get(videoUrl));
+    if (rawData['statusCode']  === 404){rawData['isLSEnabled'] = 'not a Pro';}
+    console.log(rawData)
     var lightstream = rawData['isLSEnabled'];
     console.log(lightstream, rawData);
     return lightstream;
@@ -52,6 +57,16 @@ function parseApi(keys, rawData) {
         //Formatting date
         if (apiName == 'createdAt') {
             status = status.substr(0,10);
+        }
+        //Gettings groups
+        if (apiName == 'groups') {
+            var allGroups = new Array()
+            groupData = rawData.user['groups'];
+            for (group in groupData) {
+                allGroups.push(groupData[parseInt(group)].name)
+            }
+            console.log(allGroups.toString())
+            status = allGroups.toString()
         }
         //Gettings Sparks
         if (apiName == 'sparks') {
@@ -91,6 +106,7 @@ function apiRunner() {
         resetHtml('generated');
         var url = buildUrl(input);
         var rawData = JSON.parse(Get(url));
+        if (rawData["statusCode"] === 404) {alert("User not found, API returned 404");}
         var keys = 'apiList'
         parseApi(keys, rawData);
     }
